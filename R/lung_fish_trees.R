1bcc25489a0d046b739ad1bdc9361f2f2e087661
David Barnhill <davidbarnhill@Davids-MBP-2.ern.nps.edu>
2023-09-01 10:22:13 -0700

    Update.  With lungfish and leaf labels.

--- R/lung_fish_trees.R
+++ R/lung_fish_trees.R
@@ -1,7 +1,7 @@
 #' Phylogenetic trees based on lung fish data
 #'
 #' 1290 (non-equidistant) gene trees originating from lung fish data in matrix form
-#'
+#' @source Liang D, Shen XX, Zhang P. One thousand two hundred ninety nuclear genes from a genome-wide survey support lungfishes as the sister group of tetrapods. Mol Biol Evol. 2013 Aug;30(8):1803-7. doi: 10.1093/molbev/mst072. Epub 2013 Apr 14. PMID: 23589454.
 #' @format ## `Lung_Fish`
 #' A 1290 x 45 matrix where each row is an ultrametric representing a phylogenetic tree
 "Lung_Fish"
