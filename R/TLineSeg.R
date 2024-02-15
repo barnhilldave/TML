6168781f50288afa0fd8e62bcf3fb25ff26a0be3
David Barnhill <david.barnhill@nps.edu>
2024-02-15 11:33:32 -0800

    Update line segment

--- R/TLineSeg.R
+++ R/TLineSeg.R
@@ -1,9 +1,10 @@
-#' Construct a max-plus tropical line segment between two points
+#' Construct a max- or min-plus tropical line segment between two points
 #'
 #' This function constructs a max-plus tropical line segment between two points
 #'
 #' @param D1 point in the tropical projective torus
 #' @param D2 point in the tropical projective torus
+#' @param add logical; 'max' indicates a max-plus line segment and 'min' constructs a min-plus line segment
 #' @return list of points defining the tropical line segment
 #' @author Ruriko Yoshida \email{ryoshida@@nps.edu}
 #' @export
@@ -11,21 +12,36 @@
 #'D1 <-c(0,4,2)
 #'D2 <- c(0,7,-1)
 #'TLineSeg(D1, D2)
+#'TLineSeg(D1, D2,add='min')
 
-TLineSeg <- function(D1, D2){
+TLineSeg <- function(D1, D2,add='max'){
   d <- length(D1)
   if(length(D1) != length(D2))
     warning("dimension is wrong!")
-  index <- order(D2 - D1)
-  lambda <- (D2 - D1)[index]
+
   x1 <- rep(0, d)
   segment <- list()
-  for(j in 1:d){
-    for(i in 1:d){
-      x1[i] <- 0
-      x1[i] <- max(lambda[j] + D1[i], D2[i])
+  if(add=='max'){
+    index <- order(D2 - D1)
+    lambda <- (D2 - D1)[index]
+    for(j in 1:d){
+      for(i in 1:d){
+        x1[i] <- 0
+        x1[i] <- max(lambda[j] + D1[i], D2[i])
+      }
+      segment[[j]] <- normaliz.vector(x1)
     }
-    segment[[j]] <- x1
+  }
+  if(add=='min'){
+    index <- order(D2 - D1,decreasing=TRUE)
+    lambda <- (D2 - D1)[index]
+    for(j in 1:d){
+      for(i in 1:d){
+        x1[i] <- 0
+        x1[i] <- min(lambda[j] + D1[i], D2[i])
+      }
+      segment[[j]] <- normaliz.vector(x1)
+    }
   }
   return(segment)
 }
